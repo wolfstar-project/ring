694c501d4a10ff2b428a9b0823664510406fd534
RedStar <redstar071@proton.me>
2026-01-03 14:41:46 +0100

    refactor: add taze configuration, and update TypeScript ESLint config

---
+++ taze.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'taze';
+
+export default defineConfig({
+	write: true,
+	// run `npm install` or `yarn install` right after bumping
+	install: true,
+	update: true,
+	recursive: true,
+	includeLocked: true,
+	interactive: true,
+	packageMode: {
+		typescript: 'ignore',
+		'@prisma/client': 'ignore',
+		prisma: 'ignore'
+	},
+	depFields: {
+		overrides: false
+	}
+});
